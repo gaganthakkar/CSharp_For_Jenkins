76475b0b8207d017edf195c7bb3c1d55c06a1403
gaganthakkar <gaganthakkar8@gmail.com>
2024-07-07 18:29:13 -0400

    setup the code structure

---
+++ CSharp_Jenkins/CSharp_Jenkins/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace CsharpForJenkins
+{
+    class CJenkins
+    {
+        public static void Main()
+        {
+            Console.WriteLine("envirnment has been setup");
+        }
+    }
+}
