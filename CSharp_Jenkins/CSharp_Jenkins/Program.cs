bd8652315857b9b87bf803c651fe096015003356
gaganthakkar <gaganthakkar8@gmail.com>
2024-07-07 19:33:49 -0400

    created list of employees

--- CSharp_Jenkins/CSharp_Jenkins/Program.cs
+++ CSharp_Jenkins/CSharp_Jenkins/Program.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using CSharp_Jenkins;
+using System;
 
 namespace CsharpForJenkins
 {
@@ -6,7 +7,39 @@ namespace CsharpForJenkins
     {
         public static void Main()
         {
-            Console.WriteLine("envirnment has been setup");
+            Console.WriteLine("envirnment has been setup"); 
+
+
+            Employee employee1 = new Employee{
+
+                Id = 1, 
+                Name = "Gagan",
+                Hobbies = new List<string> { "cricket", "badminton"}
+            }; 
+            
+            Employee employee2 = new Employee{
+
+                Id = 2, 
+                Name = "Prarthana",
+                Hobbies = new List<string> { "swimming", "badminton"}
+            };
+
+            List<Employee> employees = new List<Employee>
+            {
+                employee1, 
+                employee2
+            };
+
+            foreach (var item in employees)
+            {
+                    Console.WriteLine($"{item.Id}, {item.Name}");
+                Console.WriteLine("Hobbies");
+
+                foreach (var hobby in item.Hobbies)
+                {
+                    Console.WriteLine($"{hobby}");
+                }
+            }
         }
     }
 }
