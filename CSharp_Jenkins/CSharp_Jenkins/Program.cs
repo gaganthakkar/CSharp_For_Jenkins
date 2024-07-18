cc687aedb020ac7bcefd700406664ed8267cbea4
gaganthakkar <gaganthakkar8@gmail.com>
2024-07-18 09:32:13 -0400

    returning type class in the method

--- CSharp_Jenkins/CSharp_Jenkins/Program.cs
+++ CSharp_Jenkins/CSharp_Jenkins/Program.cs
@@ -7,39 +7,61 @@ namespace CsharpForJenkins
     {
         public static void Main()
         {
-            Console.WriteLine("envirnment has been setup"); 
+            //Console.WriteLine("envirnment has been setup"); 
 
 
-            Employee employee1 = new Employee{
+            //Employee employee1 = new Employee{
 
-                Id = 1, 
-                Name = "Gagan",
-                Hobbies = new List<string> { "cricket", "badminton"}
-            }; 
+            //    Id = 1, 
+            //    Name = "Gagan",
+            //    Hobbies = new List<string> { "cricket", "badminton"}
+            //}; 
             
-            Employee employee2 = new Employee{
+            //Employee employee2 = new Employee{
 
-                Id = 2, 
-                Name = "Prarthana",
-                Hobbies = new List<string> { "swimming", "badminton"}
-            };
+            //    Id = 2, 
+            //    Name = "Prarthana",
+            //    Hobbies = new List<string> { "swimming", "badminton"}
+            //};
 
-            List<Employee> employees = new List<Employee>
-            {
-                employee1, 
-                employee2
-            };
+            //List<Employee> employees = new List<Employee>
+            //{
+            //    employee1, 
+            //    employee2
+            //};
 
-            foreach (var item in employees)
-            {
-                    Console.WriteLine($"{item.Id}, {item.Name}");
-                Console.WriteLine("Hobbies");
+            //foreach (var item in employees)
+            //{
+            //        Console.WriteLine($"{item.Id}, {item.Name}");
+            //    Console.WriteLine("Hobbies");
 
-                foreach (var hobby in item.Hobbies)
-                {
-                    Console.WriteLine($"{hobby}");
-                }
-            }
+            //    foreach (var hobby in item.Hobbies)
+            //    {
+            //        Console.WriteLine($"{hobby}");
+            //    }
+            //}
+
+            Triangle triangle = new Triangle();
+            Triangle returnVal = triangle.GettingInstance(true);
+
+            Console.WriteLine(returnVal.color);
+      
         }
     }
+
+    public class Triangle
+    {
+        public string color { get; set; }
+
+        public Triangle GettingInstance(bool isBool)
+        {
+            Triangle returnTriangleColor;
+
+            returnTriangleColor = new Triangle { color = "green" };
+
+            return returnTriangleColor;
+        }
+
+    }
+
 }
